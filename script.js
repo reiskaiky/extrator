bd531e81fac61a77cda5270a0b1ae17a0000117b
reiskaiky <reis.kaiky@escola.pr.gov.br>
2025-06-24 08:36:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave') 
+
+botaoMostraPalavras.addeventlistener('click',mostraPalavrasChaves);
+
+function mostraPalavraschaves(){
+    alert('fui clicado!')
+}
